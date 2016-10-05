13087c0193628b59d2a5b7061e6469cb92c0262a
Maja Engvall <majaengvall@student93-26.eduroam.uu.se>
2016-10-05 13:47:29 +0200

    Table_task works as function with worker_id and number of workers as input

--- BENCHOP/Table_task.m
+++ BENCHOP/Table_task.m
@@ -196,30 +196,30 @@ cd(rootpath);
 end
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Table2=table(tBSeuCallUI,tBSamPutUI,tBSupoutCallI,tBSeuCallUII,tBSamPutUII,tBSupoutCallII,'RowNames',Methods)
-err=[rBSeuCallUI,rBSamPutUI,rBSupoutCallI,rBSeuCallUII,rBSamPutUII,rBSupoutCallII];
-err=round(log10(err));
+%Table2=table(tBSeuCallUI,tBSamPutUI,tBSupoutCallI,tBSeuCallUII,tBSamPutUII,tBSupoutCallII,'RowNames',Methods)
+%rr=[rBSeuCallUI,rBSamPutUI,rBSupoutCallI,rBSeuCallUII,rBSamPutUII,rBSupoutCallII];
+%err=round(log10(err));
 
 % Now use this table as input in our input struct:
-input.data = Table2;
-input.error = err;
+%input.data = Table2;
+%input.error = err;
 
 % Set the row format of the data values (in this example we want to use
 % integers only):
-input.dataFormat = {'%.1e'};
+%input.dataFormat = {'%.1e'};
 
 % Switch transposing/pivoting your table:
-input.transposeTable = 1;
+%input.transposeTable = 1;
 
 % Column alignment ('l'=left-justified, 'c'=centered,'r'=right-justified):
-input.tableColumnAlignment = 'c';
+%input.tableColumnAlignment = 'c';
 
 % Switch table borders on/off:
-input.tableBorders = 0;
+%input.tableBorders = 0;
 
 % Switch to generate a complete LaTex document or just a table:
-input.makeCompleteLatexDocument = 0;
+%input.makeCompleteLatexDocument = 0;
 
-latex = latexTable(input);
+%latex = latexTable(input);
 
 end
