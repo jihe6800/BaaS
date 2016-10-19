60790f8fa29061fd93451a13991439b53eabf9b7
dinotrainer <tunell.john@gmail.com>
2016-10-19 16:49:24 +0200

    Fixed code on webserver, task and run_task to work with server running locally. Seems to work

--- webserver/run_tasks.py
+++ webserver/run_tasks.py
@@ -3,7 +3,7 @@ from local_celery.tasks import getResultBench
 import json
 
 class HelloRPC(object):
-    def req_benchmark(self, num_workers):
+    def req_benchmark(self, worker, num_workers, num_solvers):
         print num_workers
         result = getResultBench.delay(1,2,3)
         result = result.get(timeout=4000)
